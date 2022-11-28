b55bde1403cb3dff0fa5eefdf7adf73b428ca3d7
journo10 <akifbgtr@gmail.com>
2022-11-28 21:41:35 +0300

    operatörler konusu

--- src/Javascript-Temelleri/Operetörler/app.js
+++ src/Javascript-Temelleri/Operetörler/app.js
@@ -24,11 +24,13 @@ sonuc /= sayi1; // sonuc = sonuc / sayi1
 sonuc %= sayi1; // sonuc = sonuc % sayi1
 
 // 3- KARŞILAŞTIRMA OPERATÖRLERİ
-sonuc = (sayi1 == sayi2)// false
-sonuc = (sayi1 === sayi2)// veri tipinede bakıyor.
-sonuc = (sayi1 != sayi2)// true
-
-
+sonuc = sayi1 == sayi2; // false
+sonuc = sayi1 === sayi2; //veri tip kontrolü de yapar.
+sonuc = sayi1 != sayi2; // true
+sonuc = sayi1 > sayi2;
+sonuc = sayi1 < sayi2;
+sonuc = sayi1 >= sayi2;
+sonuc = sayi1 <= sayi2;
 
 // 4- MANTIKSAL OPERATÖRLER
 
