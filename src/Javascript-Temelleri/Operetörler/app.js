2d4c67149037a4fa1759e1477894ee6f169494e5
journo10 <akifbgtr@gmail.com>
2022-11-28 21:36:48 +0300

    operatörler konusu

---
+++ src/Javascript-Temelleri/Operetörler/app.js
@@ -0,0 +1,36 @@
+let sonuc;
+
+let sayi1 = 10,
+  sayi2 = 20,
+  sayi3 = 35;
+
+// 1- ARİTMETİK OPERATÖRLER
+sonuc = sayi1 + sayi2;
+sonuc = sayi1 - sayi2;
+sonuc = sayi1 * sayi2;
+sonuc = sayi1 / sayi2;
+sonuc = sayi3 % sayi1; // mod alma kalanda kalan sayı
+//sonuc = sayi1++; // sayi1 + 1
+//sonuc = ++sayi1; // sayi1 + 1
+// sonuc = sayi1--; // sayi1 - 1
+// sonuc = --sayi1; // sayi1 - 1
+
+// 2- ATAMA OPERATÖRLERİ
+sonuc = sayi1;
+sonuc += sayi1; // sonuc = sonuc + sayi1
+sonuc -= sayi1; // sonuc = sonuc - sayi1
+sonuc *= sayi1; // sonuc = sonuc * sayi1
+sonuc /= sayi1; // sonuc = sonuc / sayi1
+sonuc %= sayi1; // sonuc = sonuc % sayi1
+
+// 3- KARŞILAŞTIRMA OPERATÖRLERİ
+sonuc = (sayi1 == sayi2)// false
+sonuc = (sayi1 === sayi2)// veri tipinede bakıyor.
+sonuc = (sayi1 != sayi2)// true
+
+
+
+// 4- MANTIKSAL OPERATÖRLER
+
+//sonuc
+console.log(sonuc);
